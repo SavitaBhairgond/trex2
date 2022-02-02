2404fdaa88b138a852f9050d7fb07d5e52ec6362
SavitaBhairgond <82020825+SavitaBhairgond@users.noreply.github.com>
2022-02-02 20:07:45 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,31 @@
+
+var trex ,trex_running;
+function preload(){
+  trex_running = loadAnimation("trex1.png", "trex3.png", "trex4.png");
+
+}
+
+function setup(){
+  createCanvas(600,200)
+  
+  //create a trex sprite
+  trex= createSprite(50,160,20,50);
+  trex.addAnimation("running",trex_running);
+  edges=createEdgeSprites();
+  trex.scale=0.5;
+  trex.x=50;
+}
+
+function draw(){
+  background("pink");
+  console.log("trex running");
+  console.log(trex.y);
+  if(keyDown("space"))
+  {
+    trex.velocityY=-10;
+  }
+  trex.velocityY=trex.velocityY+0.5;
+  trex.collide(edges[3]);
+  drawSprites();
+
+}
